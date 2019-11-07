bdd221e59ab3d93b596b8d788221538fd331f85a
Kevin Chu <kevin.git.chu@gmail.com>
2019-11-06 22:37:48 -0800

    Initial file for choose C and sigma

--- MachineLearning/machine-learning-ex6/ex6/dataset3Params.m
+++ MachineLearning/machine-learning-ex6/ex6/dataset3Params.m
@@ -11,6 +11,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -23,11 +24,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+results = zeros(8, 8);
 
+potential_C = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+potential_sigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+for i = 1:8
+	for j = 1:8
+	
+		model= svmTrain(X, y, potential_C(i), @(x1, x2) gaussianKernel(x1, x2, potential_sigma(j)));
+		predictions = svmPredict(model, Xval);
+	
+		results(i, j) = mean(double(predictions ~= yval));
+	end
+end
 
-
-
+[C, sigma] = min(results);
 
 % =========================================================================
 
